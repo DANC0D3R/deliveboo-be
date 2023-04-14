cc8a981fd7a5da74f9fa8e683ffe8aea35267cc8
1alberto994 <alby94ct@gmail.com>
2023-04-14 09:48:40 +0200

    relazione food_order completata

---
+++ database/migrations/2023_04_14_073736_create_food_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food_order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("food_id");
+            $table->foreign("food_id")->references("id")->on("foods");
+            $table->unsignedBigInteger("order_id");
+            $table->foreign("order_id")->references("id")->on("orders");
+            $table->unsignedTinyInteger('quantity');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food_order');
+    }
+};
