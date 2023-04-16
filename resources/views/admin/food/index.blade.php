7db3a4bd7bb8b3f64f2e5e165e5a5f678a81ef69
Zineb Atlassi <atlassizineb@gmail.com>
2023-04-16 17:06:57 +0200

    Add Style Food

--- resources/views/admin/food/index.blade.php
+++ resources/views/admin/food/index.blade.php
@@ -1,72 +1,106 @@
 @extends('layouts.admin')
 
 @section('content')
-@include('partials.success')
 
 <div class="container-fluid mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Menù') }}</div>
 
-                <div class="card-body">
+                <div class="success">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
                     @endif
+                </div>
+
+                <div class="text-center mb-3 title-container">
+                    <h1 class="my-3">
+                        I tuoi prodotti
+                    </h1>
 
                     {{-- Pulsante create --}}
-                    <a href="{{ route('admin.foods.create') }}" class="btn btn-success">
-                        Inserisci il tuo piatto
+                    <a href="{{ route('admin.foods.create') }}" class="btn create">
+                        <i class="fa-solid fa-plus"></i> Inserisci un nuovo piatto
                     </a>
                 </div>
+
             </div>
         </div>
     </div>
     <div class="row g-3">
             @foreach ($foods as $food)
                 <div class="col-4 mb-3">
-                    <div class="card text-center">
+                    <div class="card food-card">
                         <div class="card-body">
-                            <h2 class="card-title">{{ $food->name }}</h2> {{-- Nome piatto --}}
-                            <h6 class="card-title">{{$food->price}}€</h6> {{-- Prezzo piatto --}}
-                            <h6 class="card-title">{{$food->description}}</h6> {{-- Descrizione piatto --}}
 
-                            
-                            {{-- Pulsante show --}}
-                            <a href="{{ route('admin.foods.show', $food->id) }}" class="btn btn-primary"> 
-                                Vedi dettagli
-                            </a>
-                            {{-- Pulsante sedit --}}
-                            <a href="{{ route('admin.foods.edit', $food->id) }}" class="btn btn-warning">
-                                Aggiorna
-                            </a>
-                            
-                            {{-- eliminazione tramite modale --}}
-                            <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal_{{ $food->id }}">Elimina</button>
+                            <div>
+                                {{-- Immagine --}}
+                                <img src="https://www.cucchiaio.it/content/cucchiaio/it/ricette/2020/04/pasta-alla-zozzona/jcr:content/imagePreview.img10.jpg/1588238376193.jpg" alt="">
+                            </div>
 
-                            <div class="modal fade" id="deleteModal_{{ $food->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h1 class="modal-title fs-5" id="exampleModalLabel">Elimminazione piatto</h1>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <div>
+                                {{-- Nome piatto --}}
+                                <h3 class="card-title">{{ $food->name }}</h3>
+            
+                                {{-- Prezzo piatto --}}
+                                <p class="card-title">{{$food->price}}€</p>
+
+                                {{-- Descrizione piatto --}}
+                                <p class="card-title mb-2">{{$food->description}}</p>
+
+                                {{-- Disponibilità del piatto --}}
+                                <div class="disponibilità mt-1">
+                                    @if ($food->availability == true)
+                                        <p class="green"><strong>
+                                            Disponibile
+                                        </strong></p>
+                                    @else
+                                        <p class="red"><strong>
+                                            Non Disponibile
+                                        </strong></p>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="button-container d-flex my-2">
+
+                                {{-- Pulsante show --}}
+                                <a href="{{ route('admin.foods.show', $food->id) }}" class="crud-button show"> 
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                {{-- Pulsante sedit --}}
+                                <a href="{{ route('admin.foods.edit', $food->id) }}" class="crud-button edit">
+                                    <i class="fa-solid fa-pen"></i>
+                                </a>
+                            
+                                {{-- eliminazione tramite modale --}}
+                                <button class="crud-button delete" data-bs-toggle="modal" data-bs-target="#deleteModal_{{ $food->id }}">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                </button>
+
+                                <div class="modal fade" id="deleteModal_{{ $food->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Elimminazione piatto</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Confermi di voler eliminare {{ $food->title }}?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                            <form action="{{ route('admin.foods.destroy', $food->id) }}" method="POST" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                @csrf
+                                                @method('DELETE')
+            
+                                                {{-- Pulsante delete --}}
+                                                <button class="btn btn-danger">Elimina</button>
+                                            </form>
+                                        </div>
                                     </div>
-                                    <div class="modal-body">
-                                        Confermi di voler eliminare {{ $food->title }}?
                                     </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                                        <form action="{{ route('admin.foods.destroy', $food->id) }}" method="POST" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                            @csrf
-                                            @method('DELETE')
-        
-                                            {{-- Pulsante delete --}}
-                                            <button class="btn btn-danger">Elimina</button>
-                                        </form>
-                                    </div>
-                                  </div>
                                 </div>
                             </div>
                         </div>
@@ -76,3 +110,79 @@
         </div>
 </div>
 @endsection
+
+<style scoped>
+
+    /* TITOLO + CREATE */
+    .title-container .create{
+        background-color: #FF8400 !important;
+    }
+
+    .title-container a{
+        color: white;
+    }
+
+    .title-container a:hover{
+        background-color: #FFA559 !important;
+        color: white;
+    }
+
+    /* CARDS */
+
+    .food-card{
+        min-height: 500px;
+        cursor: pointer;
+    }
+    .food-card:hover{
+        border:#FF8400;
+        box-shadow: 0px 0px 6px 0px rgba(255,165,89,0.60);
+    }
+    .food-card .card-body{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+    img{
+        width: 100%;
+        border-radius: 15px;
+        margin-bottom: 1rem;
+    }
+    /* DISPONIBILITA */
+    .disponibilità{
+        font-size: 0.8rem;
+    }
+    .green{
+        color: green;
+    }
+    .red{
+        color: red;
+    }
+
+
+    /* CRUD BUTTON */
+    .button-container{
+        justify-content: center;
+        align-items: center;
+    }
+    .crud-button{
+        color: white;
+        margin-left: 1rem;
+        padding: 1rem;
+        text-align: center;
+        border-radius: 50%;
+        border: none;
+    }
+    .crud-button:hover{
+        color: white;
+        box-shadow: 0px 0px 6px 0px rgba(0,0,0,0.40);
+    }
+    .show{
+        background-color: #FFA559;
+    }
+    .edit{
+        background-color: #F7D060;
+    }
+    .delete{
+        background-color: red;
+    }
+</style>
