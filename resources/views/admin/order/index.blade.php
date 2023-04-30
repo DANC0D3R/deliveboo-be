bda15843516d43d460528dfb829e91bd183108b1
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-04-30 11:46:14 +0200

    Add chartjs tramite cdn

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
   <h1>Ordini</h1>
+
+  <canvas id="myChart"></canvas>
+
+  
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script type="text/javascript">
+
+    
+    let orders =  {{ Js::from($order) }};
+
+    (async function() {
+        const data = orders;
+      
+        new Chart(
+          document.getElementById('myChart'),
+          {
+            type: 'bar',
+            data: {
+              labels: data.map(row => row.created_at),
+              datasets: [
+                {
+                  label: 'Incassi per mese',
+                  data: data.map(row => row.total_price)
+                }
+              ]
+            }
+          }
+        );
+      })();
+  </script>
 @endsection
