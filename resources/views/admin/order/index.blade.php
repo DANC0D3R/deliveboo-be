db0eae38b0630607a66551f501873420753b8d48
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-04-30 12:46:15 +0200

    Update con commenti

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -10,17 +10,29 @@
   <script type="text/javascript">
 
     
-    let orders =  {{ Js::from($order) }};
+    let orders =  {{ Js::from($order) }}; //prendo la tabella orders e la salvo in una variabile
 
+    let newOrder = []; //inizializzo un array, mi servirà per raggruppare i dati con lo stesso mese
+    
     for (let i = 0; i < orders.length; i++) {
-      const newCreated = orders[i].created_at.slice(0, 7);
-      const date_key = 'date_key';
-      orders[i][date_key] = newCreated
+      const newCreated = orders[i].created_at.slice(0, 7); //formatto il created at
+      let dk = 'date_key';
+      orders[i][dk] = newCreated //ogni ordine, alla key date_key, avrà la data formattata
+      const {total_price, date_key} = orders[i]; //salvo i dati che mi servono in due variabiili
+      let formattedOrder = {}; //inizializzo il mio oggetto e lo riempio con le keys che mi servono
+      formattedOrder['price'] = total_price;
+      formattedOrder['month'] = date_key;
+
+      newOrder.push(formattedOrder); //pusho l'oggetto nell'array
     }
-    console.log('orders', orders);     
+    console.log('neworder', newOrder);
+    console.log('orders', orders);
+
+
+    
 
     (async function() {
-        const data = orders;
+        const data = orders; //per ora prendo i dati per il grafico da questa variabile, poi vorrei raggrupare i dati per mese
       
         new Chart(
           document.getElementById('myChart'),
